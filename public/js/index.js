be683161e5e9359750c10ffe93e8819a1bbf95bb
EddyKilonzo <eddymax3715@gmail.com>
2022-05-10 15:35:33 +0300

    added dom and styled buttons

--- public/js/index.js
+++ public/js/index.js
@@ -50,29 +50,4 @@ let calcScrollValue = () => {
   window.onscroll = calcScrollValue;
   window.onload = calcScrollValue;
   
-  const theme = document.querySelector(':root');
-  const btns = document.querySelectorAll('.btn');
-  
-  btns.forEach(function(btn){
-  
-      btn.addEventListener("click", function(e){
-  
-          const color = e.currentTarget.classList;
-  
-          if(color.contains("btn1")){
-              theme.style.setProperty("--theme-color", "#3498db");
-          }
-          else if(color.contains("btn2")){
-              theme.style.setProperty("--theme-color", "#ff0808");
-          }
-          else if(color.contains("btn3")){
-              theme.style.setProperty("--theme-color", "#1cb65d");
-          }
-          else if(color.contains("btn4")){
-              theme.style.setProperty("--theme-color", "#8e44ad");
-          }
-          else{
-              theme.style.setProperty("--theme-color", "#f4b932");
-          }
-      });
-  });
+
